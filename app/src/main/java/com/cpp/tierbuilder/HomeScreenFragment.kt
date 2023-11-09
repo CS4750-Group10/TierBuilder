e4306ba973741e31021b99a3e59e7c46f7d13399
n-gamez <143359387+n-gamez@users.noreply.github.com>
2023-11-09 15:35:47 -0800

    fragments and navigation
    changed activity_main into a fragment holder. added 3 fragments. (HomeScreenFragment, TierListFragment, SavedListsFragment) and their xml files. Added nav_graph and logic to navigrate through fragments.

---
+++ app/src/main/java/com/cpp/tierbuilder/HomeScreenFragment.kt
@@ -0,0 +1,33 @@
+package com.cpp.tierbuilder
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class HomeScreenFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home_screen, container, false)
+
+        val newListButton: Button = view.findViewById(R.id.newListButton)
+        val loadListButton: Button = view.findViewById(R.id.loadListButton)
+
+
+        newListButton.setOnClickListener {
+            findNavController().navigate(R.id.tierListFragment)
+        }
+
+        loadListButton.setOnClickListener {
+            findNavController().navigate(R.id.savedListsFragment)
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/cpp/tierbuilder/MainActivity.kt
+++ app/src/main/java/com/cpp/tierbuilder/MainActivity.kt
@@ -2,14 +2,10 @@ package com.cpp.tierbuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
     }
 }
