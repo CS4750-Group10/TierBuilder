9cf768b5b7367654dabffee14b4d1bc92c9b6a26
lancem9 <lance.mtz03@gmail.com>
2023-11-09 19:28:06 -0800

    added URI attribute to Item data class

--- app/src/main/java/com/cpp/tierbuilder/Item.kt
+++ app/src/main/java/com/cpp/tierbuilder/Item.kt
@@ -1,6 +1,9 @@
 package com.cpp.tierbuilder
 
+import android.net.Uri
+
 // this class is intended to represent a single image within a single tier
 data class Item(
-    val name: String
+    val name: String,
+    val imagePath: Uri
 )
