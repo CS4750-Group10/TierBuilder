cbd8f1e1a5811d23d17f7e39503f1cb8012c0bd2
lancem9 <lance.mtz03@gmail.com>
2023-11-09 17:55:00 -0800

    Tier and Item data class skeletons

---
+++ app/src/main/java/com/cpp/tierbuilder/Item.kt
@@ -0,0 +1,6 @@
+package com.cpp.tierbuilder
+
+// this class is intended to represent a single image within a single tier
+data class Item(
+    val name: String
+)
---
+++ app/src/main/java/com/cpp/tierbuilder/Tier.kt
@@ -0,0 +1,7 @@
+package com.cpp.tierbuilder
+
+data class Tier(
+    val rank: String,
+    val color: String,
+    val items: List<Item>
+)
